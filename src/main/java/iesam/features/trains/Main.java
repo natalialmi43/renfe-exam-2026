69283e534dc96893cbd794845ba6d5e96d292ad6
MCN <natalialmi43@gmail.com>
2026-05-14 16:41:28 +0200

    Implemented the layers domain, data and presentation. Created the main model 'Train', includes 4 attributes, one of that is 'id'. Created the constructor, getter, setter and toString methods. Tried if the class Train works at main

---
+++ src/main/java/iesam/features/trains/Main.java
@@ -0,0 +1,11 @@
+package iesam.features.trains;
+
+import iesam.features.trains.domain.Train;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Train train = new Train("MD5-001", "Train", "2026", "White");
+    }
+}
--- src/main/java/iesam/features/trains/domain/Train.java
+++ src/main/java/iesam/features/trains/domain/Train.java
@@ -5,4 +5,55 @@ public class Train {
     private String name;
     private String yearOfConstruction;
     private String colour;
+
+    public Train(String id, String name, String yearOfConstruction, String colour) {
+        this.id = id;
+        this.name = name;
+        this.yearOfConstruction = yearOfConstruction;
+        this.colour = colour;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYearOfConstruction() {
+        return yearOfConstruction;
+    }
+
+    public void setYearOfConstruction(String yearOfConstruction) {
+        this.yearOfConstruction = yearOfConstruction;
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Train{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", yearOfConstruction='" + yearOfConstruction + '\'' +
+                ", colour='" + colour + '\'' +
+                '}';
+    }
 }
