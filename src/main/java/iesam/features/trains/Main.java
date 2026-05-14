d404b6f3cab3ba852f3bb0ed323bc6f74d0622d7
MCN <natalialmi43@gmail.com>
2026-05-14 17:58:13 +0200

    Tried at the main the view methods

--- src/main/java/iesam/features/trains/Main.java
+++ src/main/java/iesam/features/trains/Main.java
@@ -1,11 +1,18 @@
 package iesam.features.trains;
 
 import iesam.features.trains.domain.Train;
+import iesam.features.trains.presentation.TrainView;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Train train = new Train("MD5-001", "Train", "2026", "White");
+
+        TrainView.printTrains();
+
+        TrainView.saveTrain();
+
+        TrainView.deleteTrain();
     }
 }
