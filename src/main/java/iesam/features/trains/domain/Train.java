ae575dbb57fc7c11b85dd71c8d3cb991043760db
MCN <natalialmi43@gmail.com>
2026-05-14 16:36:48 +0200

    Implemented the layers domain, data and presentation. Created the main model 'Train', includes 4 attributes, one of that is 'id'.

---
+++ src/main/java/iesam/features/trains/domain/Train.java
@@ -0,0 +1,8 @@
+package iesam.features.trains.domain;
+
+public class Train {
+    private String id;
+    private String name;
+    private String yearOfConstruction;
+    private String colour;
+}
